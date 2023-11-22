2c7d289816abb2cb7f60d20bc6cc89facf15592f
Fahad Nasir <111900566+nicenustian@users.noreply.github.com>
2023-11-22 13:55:24 +0100

    Update plot_data.py

--- plot_data.py
+++ plot_data.py
@@ -1,52 +1,11 @@
-from make_dataset import make_dataset
-import matplotlib
-import matplotlib.pyplot as plt
 import pandas as pd
-import json
-import numpy as np
 from datetime import timedelta
 
 
-font = {'family' : 'serif', 'weight' : 'normal','size' : 28}
-matplotlib.rc('font', **font)
 
-
-def plot_data(output_dir, input_time_steps, output_time_steps, 
-              validation_date, tickers, columns):
-    
-    fig, ax = plt.subplots(2, 1, figsize=(10, 8))
-    fig.subplots_adjust(wspace=0., hspace=0)
-     
-    fig2, ax2 = plt.subplots(len(tickers), 1, figsize=(20, 4*len(tickers)))
-    fig2.subplots_adjust(wspace=0., hspace=0.2)
-    
-    for ti, ticker in enumerate(tickers):
-    
-        xx, df, _,_,_,_,_ = make_dataset(output_dir, validation_date, ticker, 
-                                             columns, input_time_steps, output_time_steps)
-            
-        # Load the history from the JSON file
-        history_file_name = output_dir+ticker+'_hist.json'
-        
-        with open(history_file_name, 'r') as file:
-                history = json.load(file)
-        
-        pred_file_name = output_dir+ticker+'_pred.npy'
-    
-        with open(pred_file_name, 'rb') as f:
-                mean = np.load(f)
-                upper_1sigma = np.load(f)
-                lower_1sigma = np.load(f)
-                        
-                
-        # Plot training loss
-        ax[0].plot(history['loss'], label=ticker)
-        # Plot validation loss
-        ax[1].plot(history['val_loss'])   
-        
-        
-        ax2[ti].step(df['Date'], df['Close'], where='pre', color='black',  alpha=.6)
-            
+def plot_data(ax, output_dir, ticker, validation_date, output_time_steps, df, 
+              mean, upper_1sigma, lower_1sigma):
+                  
         # Convert the last date in the DataFrame to datetime
         last_date = pd.to_datetime(df['Date'].iloc[-1])
         
@@ -55,23 +14,29 @@ def plot_data(output_dir, input_time_steps, output_time_steps,
                                           periods=output_time_steps, freq='D')
         df = pd.concat((df, pd.DataFrame({'Date': next_120_days, 'Value': 0})), 
                            ignore_index=True)
-                
-        ax2[ti].step(df['Date'], mean, color='red', linestyle='-', alpha=.6)
-        ax2[ti].fill_between(df['Date'], upper_1sigma, y2=lower_1sigma, 
-                            color='red', alpha=.2)
-            
-        ax2[ti].text(0.85, 0.1,  ticker, fontsize=32, transform = ax2[ti].transAxes, 
-                           color='black')
-    
-    ax[1].set_xlabel('Epochs')
-    ax[1].set_ylabel('Vali. Loss')
-    ax[0].set_xticks([])
-    ax[0].set_ylabel('Loss')
-    ax[0].legend(loc='upper center', fontsize=12, handlelength=.5, 
-                 bbox_to_anchor=(0.5, 1.3), ncol=8, fancybox=True)
-    
-    fig.savefig(output_dir+'loss'+'.jpg', format='jpg', dpi=300, bbox_inches = 'tight')
-    fig2.savefig(output_dir+'pred'+'.jpg', format='jpg', dpi=300, bbox_inches = 'tight')
+        validation_date_datetime = pd.to_datetime(validation_date) 
 
-    
-    
+        df_train_plot = df[df['Date']<validation_date_datetime]
+        df_validate_plot = df[df['Date']>=validation_date_datetime]
+        
+
+        ax.step(df_train_plot['Date'], df_train_plot['Close'], where='pre', 
+                      color='black', alpha=1)
+
+        ax.step(df_validate_plot['Date'], df_validate_plot['Close'], where='pre', 
+                      color='red', alpha=1)
+            
+        ax.step(df_train_plot['Date'], mean[:len(df_train_plot)], 
+                     color='black', linestyle='-', alpha=.6)
+        ax.fill_between(df_train_plot['Date'], upper_1sigma[:len(df_train_plot)], 
+                             y2=lower_1sigma[:len(df_train_plot)], 
+                                 color='grey', alpha=.2)
+        
+        ax.step(df_validate_plot['Date'], mean[len(df_train_plot):], 
+                     color='red', linestyle='-', alpha=.6)
+        ax.fill_between(df_validate_plot['Date'], upper_1sigma[len(df_train_plot):], 
+                             y2=lower_1sigma[len(df_train_plot):], 
+                                 color='red', alpha=.2)        
+        
+        ax.text(0.85, 0.1,  ticker, fontsize=32, transform = ax.transAxes, 
+                               color='black')
